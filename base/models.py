0b95cde61eea4d461b89dbf2b346b475444b7388
lagisreal89 <lagisrealyt@gmail.com>
2025-02-03 01:16:07 -0500

    CRUD

--- base/models.py
+++ base/models.py
@@ -14,6 +14,9 @@ class Room(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     
+    class Meta:
+        ordering = ['-updated','-created']
+
     def __str__(self):
         return self.name
 class Message(models.Model):
