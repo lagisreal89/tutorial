93a40972c9db5c5a4b3c3aa97119a184a20af8d3
lagisreal89 <lagisrealyt@gmail.com>
2025-02-23 18:52:48 -0500

    User Login

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Room, Topic
 from .forms import RoomForm
 from django.db.models import Q
+from django.contrib.auth.models import User
 # Create your views here.
 '''
 rooms = [
@@ -10,6 +11,16 @@ rooms = [
     {'id':3,'name':'Frontend devs'},
 ]
 '''
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except:
+            ...
+    context = {}
+    return render(request, 'base/login_register.html', context)
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q))
