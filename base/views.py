4d28bd20ef6ec68a9a34b72c94b28e7b954e984c
lagisreal89 <lagisrealyt@gmail.com>
2025-02-23 19:07:58 -0500

    User Logout

--- base/views.py
+++ base/views.py
@@ -30,6 +30,10 @@ def loginPage(request):
             messages.error(request, 'Username or Password does not exist')
     context = {}
     return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q))
